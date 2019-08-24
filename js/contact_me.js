7a97042605081cb9a1127c56a7aac263d2566da6
Fabien Udriot <fabien@omic.ch>
2019-08-24 10:10:44 +0200

    Submit form by Ajax

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,6 +1,6 @@
 $(function() {
 
-  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+  $('#formContact input, #formContact textarea').jqBootstrapValidation({
     preventSubmit: true,
     submitError: function($form, event, errors) {
       // additional error messages or events
@@ -8,26 +8,21 @@ $(function() {
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
-      var name = $("input#name").val();
-      var email = $("input#email").val();
-      var phone = $("input#phone").val();
-      var message = $("textarea#message").val();
-      var firstName = name; // For Success/Failure Message
-      // Check for white space in name for Success/Fail message
-      if (firstName.indexOf(' ') >= 0) {
-        firstName = name.split(' ').slice(0, -1).join(' ');
-      }
+      // var name = $("input#name").val();
+      // var email = $("input#email").val();
+      // var phone = $("input#phone").val();
+      // var message = $("textarea#message").val();
+      var firstName = $("input#name").val(); // For Success/Failure Message
+      // // Check for white space in name for Success/Fail message
+      // if (firstName.indexOf(' ') >= 0) {
+      //   firstName = name.split(' ').slice(0, -1).join(' ');
+      // }
       $this = $("#sendMessageButton");
-      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+      $this.prop('disabled', true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
-        type: "POST",
-        data: {
-          name: name,
-          phone: phone,
-          email: email,
-          message: message
-        },
+        url: $form.attr("action"),
+        type: 'POST',
+        data: $form.serialize(),
         cache: false,
         success: function() {
           // Success message
@@ -39,17 +34,17 @@ $(function() {
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
-          $('#contactForm').trigger("reset");
+          $('#formContact').trigger("reset");
         },
         error: function() {
           // Fail message
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Désolé " + firstName + ", le serveur mail n'a pas répondu correctement."));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
-          $('#contactForm').trigger("reset");
+          $('#formContact').trigger("reset");
         },
         complete: function() {
           setTimeout(function() {
