044a84ef9b452a465445fef49eae088310fa36a5
Fabien Udriot <fabien@omic.ch>
2021-03-18 12:44:31 +0100

    chore: fix build

--- gulpfile.js
+++ gulpfile.js
@@ -75,7 +75,6 @@ function css() {
     }))
     .on("error", sass.logError)
     .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
       cascade: false
     }))
     .pipe(header(banner, {
